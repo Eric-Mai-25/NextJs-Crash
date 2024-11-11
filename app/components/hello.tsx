5b1da6950a77fcb2139f690223655651534cfe55
Eric-Mai-25 <ericmai2@gmail.com>
2024-11-11 13:15:47 -0800

    finished

---
+++ app/components/hello.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Hello(){
+    console.log("I am a client")
+
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Hello from "@/app/components/hello"
 
 export default function Home() {
+  
   return (
+    <>
     <h1>Welcome to Next.js</h1>
+    <Hello></Hello>
+    </>
   );
 }
